1b83392d9fbcb2e92fe421d417a50ce72d4f57e0
김동오 <devlordkimdo@gmail.com>
2025-04-06 22:16:06 +0900

    first commit

---
+++ src/main/java/dlk/wti/spring/DlkWtiSpringApplication.java
@@ -0,0 +1,13 @@
+package dlk.wti.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlkWtiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlkWtiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dlk/wti/spring/DlkWtiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dlk.wti.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlkWtiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
