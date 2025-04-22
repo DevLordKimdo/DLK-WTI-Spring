2310310c6e84ec8532b16d0ec16bda7e4c867036
김동오 <devlordkimdo@gmail.com>
2025-04-22 16:46:27 +0900

    PRG패턴 기능 추가

---
+++ src/main/java/dlk/wti/spring/be/http/prgpattern/BeHttpPrgpatternController.java
@@ -0,0 +1,34 @@
+package dlk.wti.spring.be.http.prgpattern;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class BeHttpPrgpatternController {
+	
+    @GetMapping({"/be/http/prg-pattern","/be/http/prg-pattern/index"})
+	public String index(ModelMap modelMap, Model model) {
+    	
+    	// 다른 방식으로 메소드 파라미터에 ModelMap 대신 @ModelAttribute("") 를 사용 가능.
+    	// 예시 : public String index(@ModelAttribute("value") String value, Model model) {
+    	
+    	model.addAttribute("value", modelMap.get("value"));
+    	
+    	return "be/http/prgpattern/index";
+    }
+    
+    @PostMapping("/be/http/prg-pattern/submit-post")
+	public String submitPost(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    	
+    	redirectAttributes.addFlashAttribute("value", request.getParameter("value"));
+    	
+    	return "redirect:/be/http/prg-pattern/index";
+    }
+
+}
