a2c001d91e9682e5d9907c3d53158b7127e9ca91
김동오 <devlordkimdo@gmail.com>
2025-04-22 15:16:23 +0900

    rowsubmit form수정(trTemplate을 form 밖으로 분리시킴), rowsubmit formfetch 추가

--- src/main/java/dlk/wti/spring/fe/form/rowsubmit/FeFormRowsubmitController.java
+++ src/main/java/dlk/wti/spring/fe/form/rowsubmit/FeFormRowsubmitController.java
@@ -5,7 +5,9 @@ import java.util.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import dlk.wti.spring.db.crud.dto.DbCrudDTO;
 
@@ -22,17 +24,39 @@ public class FeFormRowsubmitController {
 	}
 	
 	@PostMapping("/fe/form/row-submit/submit")
-	public String Submit(@RequestParam("title")   List<String> title, 
+	public String submit(@RequestParam("title")   List<String> title, 
 			             @RequestParam("name")    List<String> name,
 			             @RequestParam("content") List<String> content) {
 		
-		List<DbCrudDTO> DbCrudDTO = feFormRowsubmitService.submit(title, name, content);
+		List<DbCrudDTO> dbCrudDTO = feFormRowsubmitService.submit(title, name, content);
 
-		for(DbCrudDTO list : DbCrudDTO) {
+		for(DbCrudDTO list : dbCrudDTO) {
 			System.out.println(list.getTitle() + " " + list.getName() + " " + list.getContent());
 		}
 		
 		return "redirect:/fe/form/row-submit/form";
 	}
-
+	
+	@GetMapping("/fe/form/row-submit/form-fetch")
+	public String formFetch() {
+		
+		return "fe/form/rowsubmit/formfetch";
+	}
+	
+	
+	@PostMapping("/fe/form/row-submit/submit-fetch")
+	@ResponseBody
+	public String submitFetch(@RequestBody Map<String, List<DbCrudDTO>> request) {
+		
+		List<DbCrudDTO> dbCrudDTO = request.get("list");
+		for(DbCrudDTO list : dbCrudDTO) {
+			System.out.println(list.getTitle() + " " + list.getName() + " " + list.getContent());
+		}
+	    
+	    return "Success";
+	}
 }
+
+// JSON 타입으로 전송할 때 백엔드 컨트롤러 에서 JSON 타입의 데이터를 받는 방법
+// @RequestBody Map<String, List<Map<String, String>>> request -> JSON 으로 들어온 데이터들을 틀 없이 받아들이기. DTO가 없을 때 임시방편으로 사용 가능.
+// @RequestBody Map<String, List<DbCrudDTO>> request -> JSON으로 들어온 데이터들을 DTO 형식으로 받아주기
