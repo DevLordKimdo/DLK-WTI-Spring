ce572283383e98bb49f771edb38cfa392842b5af
Kimdo <DevlordKimdo@gmail.com>
2025-10-04 16:20:12 +0900

    pattern wizard 수정

--- src/main/java/dlk/wti/spring/logic/pattern/wizard/LogicPatternWizardController.java
+++ src/main/java/dlk/wti/spring/logic/pattern/wizard/LogicPatternWizardController.java
@@ -1,13 +1,14 @@
 package dlk.wti.spring.logic.pattern.wizard;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import dlk.wti.spring.db.crud.dto.DbCrudDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+
 
 @Controller
 public class LogicPatternWizardController {
@@ -15,10 +16,92 @@ public class LogicPatternWizardController {
     @GetMapping({"/tmpl/logic/pattern/wizard","/tmpl/logic/pattern/wizard/index"})
 	public String index() {
     	
-
     	return "logic/pattern/wizard/index";
     }
+	
+    // step1
+    @GetMapping("/tmpl/logic/pattern/wizard/step1")
+	public String step1(HttpSession session, Model model) {
+    	
+    	model.addAttribute("username", session.getAttribute("username"));
+    	
+    	return "logic/pattern/wizard/step1";
+    }
     
+    @PostMapping("/tmpl/logic/pattern/wizard/step1/back")
+	public String step1back() {
+    	
+    	return "redirect:/tmpl" + "/logic/pattern/wizard/index";
+    }
+    
+    @PostMapping("/tmpl/logic/pattern/wizard/step1/next")
+	public String step1next(HttpSession session, DbCrudDTO dbCrudDTO) {
+    	
+    	session.setAttribute("username", dbCrudDTO.getUsername());
+    	
+    	return "redirect:/tmpl" + "/logic/pattern/wizard/step2";
+    }
+    
+
+    // step2
+    @GetMapping("/tmpl/logic/pattern/wizard/step2")
+	public String step2(HttpSession session, Model model) {
+    	
+    	if(session.getAttribute("username") == null) {
+    		return "redirect:/tmpl" + "/logic/pattern/wizard/index";
+    	}
+    	
+    	model.addAttribute("title", session.getAttribute("title"));
+    	
+    	return "logic/pattern/wizard/step2";
+    }
+    
+    @PostMapping("/tmpl/logic/pattern/wizard/step2/back")
+	public String step2back() {
+    	
+    	return "redirect:/tmpl" + "/logic/pattern/wizard/step1";
+    }
+    
+    @PostMapping("/tmpl/logic/pattern/wizard/step2/next")
+	public String step2next(HttpSession session, DbCrudDTO dbCrudDTO) {
+    	
+    	session.setAttribute("title", dbCrudDTO.getTitle());
+    	
+    	return "redirect:/tmpl" + "/logic/pattern/wizard/step3";
+    }
+    
+    // step3
+    @GetMapping("/tmpl/logic/pattern/wizard/step3")
+	public String step3(HttpSession session) {
+    	
+    	if(session.getAttribute("username") == null || session.getAttribute("title") == null) {
+    		return "redirect:/tmpl" + "/logic/pattern/wizard/index";
+    	}
+    	
+    	return "logic/pattern/wizard/step3";
+    }
+    
+    @PostMapping("/tmpl/logic/pattern/wizard/step3/back")
+	public String step3back() {
+    	
+    	return "redirect:/tmpl" + "/logic/pattern/wizard/step2";
+    }
+    
+    @PostMapping("/tmpl/logic/pattern/wizard/step3/submit")
+	public String step3submit(HttpSession session, DbCrudDTO dbCrudDTO) {
+    	
+    	System.out.println("username : " + session.getAttribute("username"));
+    	System.out.println("   title : " + session.getAttribute("title"));
+    	System.out.println(" content : " + dbCrudDTO.getContent());
+		session.invalidate();
+		
+    	return "redirect:/tmpl" + "/logic/pattern/wizard/index";
+    }
+    
+	
+/*
+url 파라미터 값과 함께 get으로 요청 받으면
+url 파라미터 값(?param=value) 이런 값들을 세션기록 및 제거 후 리다이렉트 시키는 구조.
     @GetMapping("/tmpl/logic/pattern/wizard/step1")
 	public String step1() {
     	
@@ -68,4 +151,6 @@ public class LogicPatternWizardController {
 
     	return "redirect:/tmpl" + "/logic/pattern/wizard/index";
     }
+*/
 }
+
