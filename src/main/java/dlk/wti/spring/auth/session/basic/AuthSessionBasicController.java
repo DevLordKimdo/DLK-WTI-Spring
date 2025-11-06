03ccb679bcc4c5ca8730b92fdf33154d50eaecb2
김동오 <devlordkimdo@gmail.com>
2025-11-06 17:55:09 +0900

    Auth Session Rest Controller 추가

--- src/main/java/dlk/wti/spring/auth/session/basic/AuthSessionBasicController.java
+++ src/main/java/dlk/wti/spring/auth/session/basic/AuthSessionBasicController.java
@@ -40,7 +40,4 @@ public class AuthSessionBasicController {
 		
 		return "redirect:/tmpl" + "/auth/session/basic/index";
 	}
-	
-	
-
 }
---
+++ src/main/java/dlk/wti/spring/auth/session/basic/AuthSessionBasicRestController.java
@@ -0,0 +1,39 @@
+package dlk.wti.spring.auth.session.basic;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000",
+        maxAge = 3600,
+        allowCredentials = "true")
+@RestController
+public class AuthSessionBasicRestController {
+
+    private final AuthSessionBasicService authSessionBasicService;
+    public AuthSessionBasicRestController(AuthSessionBasicService authSessionBasicService) {this.authSessionBasicService = authSessionBasicService;}
+
+    @PostMapping("/rest/auth/session/basic/request-session")
+    public ResponseEntity<String> requestSession(HttpServletRequest request) {
+        authSessionBasicService.requestSession(request);
+
+        return ResponseEntity.ok("Success");
+    }
+
+    @GetMapping("/rest/auth/session/basic/check-session")
+    public ResponseEntity<String> checkSession(HttpServletRequest request) {
+        authSessionBasicService.checkSession(request);
+
+        return ResponseEntity.ok("Success");
+    }
+
+    @PostMapping("/rest/auth/session/basic/delete-session")
+    public ResponseEntity<String> deleteSession(HttpServletRequest request) {
+        authSessionBasicService.deleteSession(request);
+
+        return ResponseEntity.ok("Success");
+    }
+}
